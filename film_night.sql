067df7cace9a19242efbd8fcf24fe1914918c6c5
boribet <boribetlen@gmail.com>
2017-10-23 16:48:03 +0100

    Updated with the sample data queries
    .

--- film_night.sql
+++ film_night.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 23, 2017 at 03:05 PM
+-- Generation Time: Oct 23, 2017 at 05:46 PM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,18 +19,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `film_night`
 --
-
--- --------------------------------------------------------
-
---
--- Table structure for table `booking`
---
-
-CREATE TABLE IF NOT EXISTS `booking` (
-  `user_id` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL,
-  `ticket_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -89,9 +77,9 @@ CREATE TABLE IF NOT EXISTS `feedback` (
 
 CREATE TABLE IF NOT EXISTS `film` (
 `film_id` int(11) NOT NULL,
-  `lenght` time NOT NULL,
-  `imdb_rating` int(11) NOT NULL,
-  `decription` varchar(255) NOT NULL,
+  `length` int(11) NOT NULL,
+  `imdb_rating` float NOT NULL,
+  `description` varchar(255) NOT NULL,
   `title` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
@@ -117,6 +105,18 @@ CREATE TABLE IF NOT EXISTS `location` (
   `location_name` varchar(255) NOT NULL,
   `capacity` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `reservation`
+--
+
+CREATE TABLE IF NOT EXISTS `reservation` (
+  `user_id` int(11) NOT NULL,
+  `event_id` int(11) NOT NULL,
+  `ticket_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -222,3 +222,117 @@ MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+
+INSERT INTO user (username, password, email, first_name, last_name)
+VALUES ('denis', 'd45', 'denis@gmail.com', 'Denis', 'Suarez');
+INSERT INTO user (username, password, email, first_name, last_name)
+VALUES ('joe', 'j35', 'joe@gmail.com', 'Joe', 'Keane');
+INSERT INTO user (username, password, email, first_name, last_name)
+VALUES ('barb', 'barbara33', 'barb@gmail.com', 'Barbara', 'Smith');
+INSERT INTO user (username, password, email, first_name, last_name)
+VALUES ('suzz', 'asdfasd', 'suzy@gmail.com', 'Suzanne', 'Stark');
+
+INSERT INTO category (category)
+VALUES ('Animation');
+INSERT INTO category (category)
+VALUES ('Drama');
+INSERT INTO category (category)
+VALUES ('Documentary');
+INSERT INTO category (category)
+VALUES ('Horror');
+INSERT INTO category (category)
+VALUES ('Action');
+INSERT INTO category (category)
+VALUES ('Adventure');
+
+INSERT INTO film_category
+VALUES(‘1’,’5’);
+INSERT INTO film_category
+VALUES(‘2’,’1’);
+INSERT INTO film_category
+VALUES(‘3’,’4’);
+INSERT INTO film_category
+VALUES(‘4’,’3’);
+
+INSERT INTO film (‘film_id’, ‘length’, ‘imdb_rating’, ‘description’, ‘title’)
+VALUES (NULL, ‘121’, ‘6.4’, ‘War between Germany and France.’, ‘The Great War 2’);
+INSERT INTO film (‘film_id’, ‘length’, ‘imdb_rating’, ‘description’, ‘title’)
+VALUES (NULL, ‘68’, ‘7.5’, ‘Toys break out to explore the outside world.’, ‘Toys 2’);
+INSERT INTO film (‘film_id’, ‘length’, ‘imdb_rating’, ‘description’, ‘title’)
+VALUES (NULL, ‘89’, ‘7.9’, ‘Zombies attack Texas.’, ‘Horror in Texas 3’);
+INSERT INTO film (‘film_id’, ‘length’, ‘imdb_rating’, ‘description’, ‘title’)
+VALUES (NULL, ‘147’, ‘9.4’, ‘A quick tour around the ancient Greece.’, ‘Ancient Greece’);
+
+INSERT INTO event(event_name, location_id, event_time, film_id)
+VALUES(‘Nachos and Horror Night’, ‘4’, '2017-11-01 17:30:00', ‘1’)
+INSERT INTO event(event_name, location_id, event_time, film_id)
+VALUES(‘Fun times with a nice animated film! ’, ‘3’, '2017-11-01 17:30:00', ‘2’)
+INSERT INTO event(event_name, location_id, event_time, film_id)
+VALUES(‘Scary things and popcorn!’, ‘2’, '2017-10-21 14:45:00', ‘3’)
+INSERT INTO event(event_name, location_id, event_time, film_id)
+VALUES(‘Greek night!’, ‘1’, '2017-11-01 17:30:00', ‘4’)
+
+Sample data: event host
+INSERT INTO event_host
+VALUES(‘1’,’4’);
+INSERT INTO event_host
+VALUES(‘2’,’3’);
+INSERT INTO event_host
+VALUES(‘3’,’2’);
+INSERT INTO event_host
+VALUES(‘4’,’1’);
+
+#so far location capaity is restricted to 2
+INSERT INTO location(location_name, capacity)
+VALUES(‘Grand Hall’, ‘2’);
+INSERT INTO location(location_name, capacity)
+VALUES(‘Medical Sciences Student Screening Room’, ‘2’);
+INSERT INTO location(location_name, capacity)
+VALUES(‘UCL Screening Room’, ‘2’);
+INSERT INTO location(location_name, capacity)
+VALUES(‘Birckbeck Open Space’, ‘2’);
+
+INSERT INTO feedback(user_id, event_id, rating, notes)
+VALUES(‘1’, ‘1’, ‘5’, ‘The food was amazing!’)
+INSERT INTO feedback(user_id, event_id, rating, notes)
+VALUES(‘3’, ‘2’, ‘5’, ‘I hated the location and the host was rude!’)
+
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-22 00:00:00', ‘5’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-23 20:30:00', ‘5’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-22 10:34:50', ‘8’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-21 19:08:09', ‘8’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-20 16:48:60', ‘3’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-20 12:17:40', ‘3’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-19 17:00:50', ‘4’);
+INSERT INTO ticket(purhcase, price)
+VALUES('2017-10-22 11:50:40', ‘4’);
+
+
+
+INSERT INTO booking
+VALUES (‘1’, ‘2’, ‘1’)
+INSERT INTO booking
+VALUES (‘4’, ‘2’, ‘2’)
+INSERT INTO booking
+VALUES (‘2’, ‘3’, ‘3’)
+INSERT INTO booking
+VALUES (‘4’, ‘3’, ‘4’)
+INSERT INTO booking
+VALUES (‘3’, ‘4’, ‘5’)
+INSERT INTO booking
+VALUES (‘1’, ‘4’, ‘6’)
+INSERT INTO booking
+VALUES (‘4’, ‘1’, ‘7’)
+INSERT INTO booking
+VALUES (‘2’, ‘1’, ‘8’)
+
+
+
