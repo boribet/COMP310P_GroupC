982052752dbcf0d2cb391843fe5c3c8c70fd13d9
boribet <boribetlen@gmail.com>
2017-12-01 10:33:40 +0000

    One to one dependency tables removed

--- film_night.sql
+++ film_night.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Oct 25, 2017 at 12:46 PM
+-- Generation Time: Dec 01, 2017 at 11:31 AM
 -- Server version: 5.6.20
 -- PHP Version: 5.5.15
 
@@ -19,6 +19,7 @@ SET time_zone = "+00:00";
 --
 -- Database: `film_night`
 --
+
 CREATE DATABASE film_night;
 USE film_night;
 
@@ -54,64 +55,24 @@ INSERT INTO `category` (`category_id`, `category`) VALUES
 CREATE TABLE IF NOT EXISTS `event` (
 `event_id` int(11) NOT NULL,
   `event_name` varchar(255) NOT NULL,
+  `type_id` int(11) NOT NULL,
   `location_id` int(11) NOT NULL,
   `event_time` datetime NOT NULL,
   `event_length` time NOT NULL,
   `film_id` int(11) NOT NULL,
-  `ticket_number` int(11) NOT NULL
+  `ticket_number` int(11) NOT NULL,
+  `hostuser_id` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `event`
 --
 
-INSERT INTO `event` (`event_id`, `event_name`, `location_id`, `event_time`, `event_length`, `film_id`, `ticket_number`) VALUES
-(1, 'Nachos and Horror Night', 4, '2017-11-01 17:30:00', '04:00:00', 1, 2),
-(2, 'Fun times with a nice animated film!', 3, '2017-11-01 03:30:00', '02:30:00', 2, 2),
-(3, 'Scary things and popcorn!', 2, '2017-10-21 14:45:00', '01:45:00', 3, 2),
-(4, 'Greek night!', 1, '2017-11-01 17:30:00', '02:30:00', 4, 2);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `event_host`
---
-
-CREATE TABLE IF NOT EXISTS `event_host` (
-  `user_id` int(11) NOT NULL,
-  `event_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `event_host`
---
-
-INSERT INTO `event_host` (`user_id`, `event_id`) VALUES
-(1, 4),
-(2, 3),
-(3, 2),
-(4, 1);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `event_type`
---
-
-CREATE TABLE IF NOT EXISTS `event_type` (
-  `event_id` int(11) NOT NULL,
-  `type_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `event_type`
---
-
-INSERT INTO `event_type` (`event_id`, `type_id`) VALUES
-(1, 1),
-(2, 2),
-(3, 2),
-(4, 3);
+INSERT INTO `event` (`event_id`, `event_name`, `type_id`, `location_id`, `event_time`, `event_length`, `film_id`, `ticket_number`, `hostuser_id`) VALUES
+(1, 'Nachos and Horror Night', 3, 4, '2017-11-01 17:30:00', '04:00:00', 1, 2, 2),
+(2, 'Fun times with a nice animated film!', 2, 3, '2017-11-01 03:30:00', '02:30:00', 2, 2, 1),
+(3, 'Scary things and popcorn!', 1, 2, '2017-10-21 14:45:00', '01:45:00', 3, 2, 3),
+(4, 'Greek night!', 1, 1, '2017-11-01 17:30:00', '02:30:00', 4, 2, 4);
 
 -- --------------------------------------------------------
 
@@ -146,39 +107,19 @@ CREATE TABLE IF NOT EXISTS `film` (
   `length` int(11) NOT NULL,
   `imdb_rating` float NOT NULL,
   `description` varchar(255) NOT NULL,
-  `title` varchar(255) NOT NULL
+  `title` varchar(255) NOT NULL,
+  `category_id` int(11) NOT NULL
 ) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=5 ;
 
 --
 -- Dumping data for table `film`
 --
 
-INSERT INTO `film` (`film_id`, `length`, `imdb_rating`, `description`, `title`) VALUES
-(1, 121, 6.4, 'War between Germany and France.', 'The Great War 2'),
-(2, 68, 7.5, 'Toys break out to explore the outside world.', 'Toys 2'),
-(3, 89, 7.9, 'Zombies attack Texas.', 'Horror in Texas 3'),
-(4, 147, 9.4, 'A quick tour around the ancient Greece.', 'Ancient Greece');
-
--- --------------------------------------------------------
-
---
--- Table structure for table `film_category`
---
-
-CREATE TABLE IF NOT EXISTS `film_category` (
-  `film_id` int(11) NOT NULL,
-  `category_id` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
---
--- Dumping data for table `film_category`
---
-
-INSERT INTO `film_category` (`film_id`, `category_id`) VALUES
-(1, 5),
-(2, 1),
-(3, 4),
-(4, 3);
+INSERT INTO `film` (`film_id`, `length`, `imdb_rating`, `description`, `title`, `category_id`) VALUES
+(1, 121, 6.4, 'War between Germany and France.', 'The Great War 2', 3),
+(2, 68, 7.5, 'Toys break out to explore the outside world.', 'Toys 2', 2),
+(3, 89, 7.9, 'Zombies attack Texas.', 'Horror in Texas 3', 4),
+(4, 147, 9.4, 'A quick tour around the ancient Greece.', 'Ancient Greece', 5);
 
 -- --------------------------------------------------------
 
@@ -200,7 +141,7 @@ INSERT INTO `location` (`location_id`, `location_name`, `capacity`) VALUES
 (1, 'Grand Hall', 2),
 (2, 'Medical Sciences Student Screening Room', 2),
 (3, 'UCL Screening Room', 2),
-(4, 'Birckbeck Open Space', 0);
+(4, 'Birckbeck Open Space', 4);
 
 -- --------------------------------------------------------
 
@@ -211,48 +152,23 @@ INSERT INTO `location` (`location_id`, `location_name`, `capacity`) VALUES
 CREATE TABLE IF NOT EXISTS `reservation` (
   `user_id` int(11) NOT NULL,
   `event_id` int(11) NOT NULL,
-  `ticket_id` int(11) NOT NULL
+  `purchase_date` date NOT NULL,
+  `price` double NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 --
 -- Dumping data for table `reservation`
 --
 
-INSERT INTO `reservation` (`user_id`, `event_id`, `ticket_id`) VALUES
-(1, 2, 1),
-(4, 2, 2),
-(2, 3, 3),
-(4, 3, 4),
-(3, 4, 5),
-(1, 4, 6),
-(4, 1, 7),
-(2, 1, 8);
-
--- --------------------------------------------------------
-
---
--- Table structure for table `ticket`
---
-
-CREATE TABLE IF NOT EXISTS `ticket` (
-`ticket_id` int(11) NOT NULL,
-  `purchase` date NOT NULL,
-  `price` int(11) NOT NULL
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=9 ;
-
---
--- Dumping data for table `ticket`
---
-
-INSERT INTO `ticket` (`ticket_id`, `purchase`, `price`) VALUES
-(1, '2017-10-22', 5),
-(2, '2017-10-23', 5),
-(3, '2017-10-22', 8),
-(4, '2017-10-21', 8),
-(5, '0000-00-00', 3),
-(6, '2017-10-20', 3),
-(7, '2017-10-19', 4),
-(8, '2017-10-22', 4);
+INSERT INTO `reservation` (`user_id`, `event_id`, `purchase_date`, `price`) VALUES
+(1, 2, '0000-00-00', 0),
+(4, 2, '0000-00-00', 0),
+(2, 3, '0000-00-00', 0),
+(4, 3, '0000-00-00', 0),
+(3, 4, '0000-00-00', 0),
+(1, 4, '0000-00-00', 0),
+(4, 1, '0000-00-00', 0),
+(2, 1, '0000-00-00', 0);
 
 -- --------------------------------------------------------
 
@@ -328,12 +244,6 @@ ALTER TABLE `location`
  ADD PRIMARY KEY (`location_id`);
 
 --
--- Indexes for table `ticket`
---
-ALTER TABLE `ticket`
- ADD PRIMARY KEY (`ticket_id`);
-
---
 -- Indexes for table `type`
 --
 ALTER TABLE `type`
@@ -369,11 +279,6 @@ MODIFY `film_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
 --
 ALTER TABLE `location`
 MODIFY `location_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=5;
---
--- AUTO_INCREMENT for table `ticket`
---
-ALTER TABLE `ticket`
-MODIFY `ticket_id` int(11) NOT NULL AUTO_INCREMENT,AUTO_INCREMENT=9;
 --
 -- AUTO_INCREMENT for table `type`
 --
